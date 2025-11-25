37f6512eb4ac43e3c9616f1ccc207f80a650fc7c
yashwinbhautoo <87039854+yashwinbhautoo@users.noreply.github.com>
2025-11-25 19:51:28 +0400

    init

--- app/Data/schema.py
+++ app/Data/schema.py
@@ -1,5 +1,8 @@
 #WEEK 8
 import sqlite3
+import pandas as pd
+from app.Data.db import connect_database
+
 def create_users_table(conn):
     """Create users table"""
     cursor = conn.cursor()
@@ -123,6 +126,29 @@ def create_it_tickets_table(conn):
     conn.commit()
     print(" it_tickets table created successfully!")
     conn.commit()
+
+def insert_dataset(dataset_name, file_name, records, description, owner):
+    conn = connect_database()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO datasets_metadata 
+        (dataset_name, file_name, records, description, owner)
+        VALUES (?, ?, ?, ?, ?)
+    """, (dataset_name, file_name, records, description, owner))
+
+    conn.commit()
+    dataset_id = cursor.lastrowid
+    conn.close()
+
+    return dataset_id
+
+
+def get_all_datasets():
+    conn = connect_database()
+    df = pd.read_sql_query("SELECT * FROM datasets_metadata", conn)
+    conn.close()
+    return df
 
 
 def create_all_tables(conn):

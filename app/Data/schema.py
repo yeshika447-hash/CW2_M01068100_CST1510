cb4ca6cb2cf548fa6d779eb9b8ced66cd4595d46
yashwinbhautoo <87039854+yashwinbhautoo@users.noreply.github.com>
2025-11-26 21:06:39 +0400

    main

--- app/Data/schema.py
+++ app/Data/schema.py
@@ -7,7 +7,7 @@ def create_users_table(conn):
     """Create users table"""
     cursor = conn.cursor()
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users(
+        (
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                    username TEXT NOT NULL UNIQUE,
                    password_hash TEXT NOT NULL,
